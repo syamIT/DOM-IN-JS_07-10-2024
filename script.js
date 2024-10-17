e820422bc76235db8097accf4180918f247696ca
syamIT <syamdonkada255@gmail.com>
2024-10-17 22:21:57 +0530

    Created And Deployed

---
+++ script.js
@@ -0,0 +1,16 @@
+let TechSkills = document.getElementById("TechSkills")
+console.log(TechSkills);
+TechSkills.innerText = "Technical Skills I have";
+console.log(TechSkills);
+TechSkills.innerHTML = "<h1>I have strong Skiils</h1>"
+
+let BootStrap = document.getElementsByClassName("BOOTSTRAP")
+console.log(BootStrap[0]);
+BootStrap[0].innerText = "BOOTSTRAP 5.0"
+
+
+let softSK = document.getElementsByTagName("h2")
+console.log(softSK[0]);
+softSK[0].innerText = "SOFT SKILLS"
+console.log(softSK);
+
